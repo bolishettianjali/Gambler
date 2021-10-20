17b8bfd8466c172e693cf474cfeedda45aa43e68
bolishettianjali <bolishettianjali@gamil.com>
2021-10-20 08:40:30 +0530

    Gambler Program

---
+++ Gambler.java
@@ -0,0 +1,8 @@
+public class Gambler{
+	static final int bet=1;
+	static int gamblerTotalStake=100;
+	public static void main(String[] args){
+	System.out.println("Bet for every game:"  +bet);
+	System.out.println("Daily Stake:" +gamblerTotalStake);
+	}
+}
